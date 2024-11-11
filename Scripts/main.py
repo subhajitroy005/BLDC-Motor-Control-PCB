85ea5c93a633494b63277ad3f30ee68bcfdb638a
Subhajit Roy <subhajitroy005@gmail.com>
2024-11-12 00:36:36 +0530

    feat: Tracks properties extracted.

--- Scripts/main.py
+++ Scripts/main.py
@@ -8,6 +8,17 @@ board_path = "../High_Amp_PCB_Designs/KiCAD_Design/High_AMP_BLDC_Control/High_AM
 
 board = LoadBoard(board_path)
 
+# --------------  Tracks -------------------
 tracks = board.Tracks()
 
-print(tracks)
+track_pos = tracks.pop()
+
+a = track_pos.GetPosition()
+
+net_1= board.FindNet("Net-(R2-Pad2)")
+
+net_info_1 = net_1.GetPosition()
+
+
+print(a)
+
