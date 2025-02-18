2136f403f62ab40118deca3eef3578b8467465fe
Subhajit Roy <subhajit.roy@fluxauto.xyz>
2025-02-18 17:43:27 +0530

    :construction: Python script ongoing.

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,26 +1,37 @@
+# 
+# import pcbnew
+# #from pcbnew import *
+# 
+# 
+# # Board Path
+# board_path = "../High_Amp_PCB_Designs/KiCAD_Design/High_AMP_BLDC_Control/High_AMP_BLDC_Control.kicad_pcb"
+# 
+# 
+# 
+# board = LoadBoard(board_path)
+# 
+# # --------------  Tracks -------------------
+# tracks = board.Tracks()
+# 
+# track_pos = tracks.pop()
+# 
+# a = track_pos.GetPosition()
+# 
+# net_1= board.FindNet("Net-(R2-Pad2)")
+# 
+# net_info_1 = net_1.GetPosition()
+# 
+# 
+# print(a)
 
-import pcbnew
-#from pcbnew import *
+import tkinter
+top = tkinter.Tk()
+
+# Code to add widgets will go here...
+top.mainloop()
 
 
-# Board Path
-board_path = "../High_Amp_PCB_Designs/KiCAD_Design/High_AMP_BLDC_Control/High_AMP_BLDC_Control.kicad_pcb"
 
 
 
-board = LoadBoard(board_path)
-
-# --------------  Tracks -------------------
-tracks = board.Tracks()
-
-track_pos = tracks.pop()
-
-a = track_pos.GetPosition()
-
-net_1= board.FindNet("Net-(R2-Pad2)")
-
-net_info_1 = net_1.GetPosition()
-
-
-print(a)
 
